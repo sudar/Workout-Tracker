2bda6ff6d271dffba30c594dd081378b2ad1edeb
Sudar <sudar@sudarmuthu.com>
2010-08-16 14:42:39 +0530

    Removed the graph view demo which was not working

--- src/com/sudarmuthu/android/workouttracker/activities/EntriesGraphActivity.java
+++ src/com/sudarmuthu/android/workouttracker/activities/EntriesGraphActivity.java
@@ -5,7 +5,6 @@ package com.sudarmuthu.android.workouttracker.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
-import arnodenhond.graphviewdemo.GraphView;
 
 /**
  * @author "Sudar Muthu (sudarm@)"
@@ -15,11 +14,5 @@ public class EntriesGraphActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		float[] values = new float[] { 2.0f,1.5f, 2.5f, 1.0f , 3.0f };
-		String[] verlabels = new String[] { "great", "ok", "bad" };
-		String[] horlabels = new String[] { "today", "tomorrow", "next week", "next month" };
-		GraphView graphView = new GraphView(this, values, "GraphViewDemo",horlabels, verlabels, GraphView.LINE);
-		setContentView(graphView);
 	}
-
 }
